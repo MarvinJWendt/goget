077ba230b6e84723e0a5d303c4bccf10f0704355
MarvinJWendt <git@marvinjwendt.com>
2022-03-16 11:38:46 +0100

    refactor: sorted modules

--- modules/modules.go
+++ modules/modules.go
@@ -7,19 +7,21 @@ type Module struct {
 }
 
 var Modules = []Module{
+	{"cobra", "github.com/spf13/cobra", Tags{CLI}},
 	{"pterm", "github.com/pterm/pterm", Tags{CLI}},
-	{"testza", "github.com/MarvinJWendt/testza", Tags{TEST_FRAMEWORK}},
-	{"fiber", "github.com/gofiber/fiber/v2", Tags{WEB_FRAMEWORK}},
-	{"survey", "github.com/AlecAivazis/survey", Tags{CLI}},
-	{"gin", "github.com/gin-gonic/gin", Tags{WEB_FRAMEWORK}},
-	{"gorm", "github.com/go-gorm/gorm", Tags{ORM}},
+	{"survey", "github.com/AlecAivazis/survey/v2", Tags{CLI}},
+	{"fyne", "github.com/fyne-io/fyne/v2", Tags{GUI}},
+	{"webview", "github.com/webview/webview", Tags{GUI}},
 	{"logrus", "github.com/sirupsen/logrus", Tags{LOGGING}},
 	{"zap", "go.uber.org/zap", Tags{LOGGING}},
 	{"zerolog", "github.com/rs/zerolog/log", Tags{LOGGING}},
-	{"cobra", "github.com/spf13/cobra", Tags{CLI}},
+	{"gorm", "github.com/go-gorm/gorm", Tags{ORM}},
 	{"testify", "github.com/stretchr/testify", Tags{TEST_FRAMEWORK}},
-	{"fyne", "github.com/fyne-io/fyne", Tags{GUI}},
-	{"webview", "github.com/webview/webview", Tags{GUI}},
+	{"testza", "github.com/MarvinJWendt/testza", Tags{TEST_FRAMEWORK}},
+	{"beego", "github.com/beego/beego/v2", Tags{WEB_FRAMEWORK}},
+	{"fasthttp", "github.com/valyala/fasthttp", Tags{WEB_FRAMEWORK}},
+	{"fiber", "github.com/gofiber/fiber/v2", Tags{WEB_FRAMEWORK}},
+	{"gin", "github.com/gin-gonic/gin", Tags{WEB_FRAMEWORK}},
 }
 
 type Tag string
